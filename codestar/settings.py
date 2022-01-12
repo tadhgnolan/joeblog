9f3d6861daaff9e67969421b6c48f281e3981c03
Tadhg N <tadhgnolan@gmail.com>
2022-01-12 22:08:48 +0000

    2nd commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = [
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
 ]
 
 
