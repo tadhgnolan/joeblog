ddbcfeab7d6ccc4876e9570eec77582c1a6a6843
Tadhg N <tadhgnolan@gmail.com>
2022-01-12 22:10:26 +0000

    3rd commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #     }
 # }
 
-DATABASES = [
-    'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
-]
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 
 # Password validation
