861d74ee0e8b451257275abbb2fb92028a0d7ec6
Tadhg N <tadhgnolan@gmail.com>
2022-01-20 13:15:26 +0000

    Add view correction 2

--- blog/views.py
+++ blog/views.py
@@ -14,8 +14,18 @@ class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
-        Post = get_object_or_404(queryset, slug=slug)
+        post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
